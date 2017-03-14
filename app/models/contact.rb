25682ccd8b1fd507c8bf71de0ec295223cf442cd
Michael <michael.ferretti@outlook.com>
2017-03-14 17:34:39 -0400

    Add method full_name

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,4 +2,8 @@ class Contact < ApplicationRecord
   def friendly_created_at
     return created_at.strftime("%A, %b %d")
   end
+
+  def full_name
+    return "#{first_name} #{last_name}"
+  end
 end
